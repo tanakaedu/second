eeba21abca935bb0351bb0a3d5a08bfd5e8e992c
YuTanaka <tanakaedu@users.noreply.github.com>
2017-07-05 11:51:05 +0900

    Update update.js

--- docs/js/update.js
+++ docs/js/update.js
@@ -16,17 +16,17 @@ function update() {
     if (game.input.mousePointer.isDown || game.input.pointer1.isDown)
     {
         //  400 is the speed it will move towards the mouse
-        game.physics.arcade.moveToPointer(sprite, 400);
+        game.physics.arcade.moveToPointer(dude, 400);
 
         //  if it's overlapping the mouse, don't move any more
-        if (Phaser.Rectangle.contains(sprite.body, game.input.x, game.input.y))
+        if (Phaser.Rectangle.contains(dude.body, game.input.x, game.input.y))
         {
-            sprite.body.velocity.setTo(0, 0);
+            dude.body.velocity.setTo(0, 0);
         }
     }
     else
     {
-        sprite.body.velocity.setTo(0, 0);
+        dude.body.velocity.setTo(0, 0);
     }
 
     
